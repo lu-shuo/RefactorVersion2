ff6826ba07f348d803489c9130f5dab6c0bcca49
chenfei <chenfei_yq@kedacom.com>
2022-05-31 16:23:13 +0800

    项目初始化

---
+++ play.js
@@ -0,0 +1,7 @@
+// 剧团的剧目数据
+
+module.exports = {
+  hamlet: { name: 'Hamlet', type: 'tragedy' },
+  'as-like': { name: 'As You Like It', type: 'comedy' },
+  othello: { name: 'Othello', type: 'tragedy' },
+};
