eae5b6a741beb872140eb5860e7d87ba1e282a2c
chenfei <chenfei_yq@kedacom.com>
2022-05-31 18:50:05 +0800

    1、分离关注点，提取swtich块函数，寻找依赖变量、将要修改的变量| 2、将amountFor作为内嵌函数，将关注点限制在statement函数作用域内

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ function statement(invoice, plays) {
   }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
+    
+    let thisAmount = amountFor(perf, play);
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add extra credit for every tencomedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    // print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+
+  function amountFor(perf, play) {
     let thisAmount = 0;
     switch (play.type) {
       case 'tragedy': // 悲剧
@@ -29,21 +44,20 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every tencomedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
 }
 
 // 用上面的数据文件（invoices.json和plays.json）作为测试输入，运行这段代码，会得到如下输出：
 const invocies = require('./invoices');
 const plays = require('./play');
 console.log(statement(invocies, plays));
+
+// 输出结果
+// Statement for BigCo
+//  Hamlet: $650.00 (55 seats)
+//  As You Like It: $580.00 (35 seats)
+//  Othello: $500.00 (40 seats)
+// Amount owed is $1,730.00
+// You earned 47 credits
+
