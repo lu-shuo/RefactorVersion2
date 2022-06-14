7e9a03050b771e95286f154799bf5a70145a4d7d
chenfei <chenfei_yq@kedacom.com>
2022-06-14 18:56:35 +0800

    替换变量

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function renderPlainText(data, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
@@ -98,7 +98,7 @@ function renderPlainText(data, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result
