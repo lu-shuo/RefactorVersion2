dbcd0b6af658868418d5592b213046fe73774567
chenfei <chenfei_yq@kedacom.com>
2022-06-29 16:20:33 +0800

    将amount函数搬移至计算器

--- createStatementData.js
+++ createStatementData.js
@@ -65,4 +65,26 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aplay;
   }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy': // 悲剧
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy': // 喜剧
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
 }
